bedef09b016fc6705ff8686b0458e30e1aed4780
saket0101 <cu.17bcs3012@gmail.com>
2021-07-19 09:30:41 +0530

    by saket

--- src/main/java/com/example/slkwebproject/HelloServlet.java
+++ src/main/java/com/example/slkwebproject/HelloServlet.java
@@ -4,12 +4,16 @@ import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
+
+
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
+
         message = "Hello World!";
+
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -23,5 +27,6 @@ public class HelloServlet extends HttpServlet {
     }
 
     public void destroy() {
+
     }
 }
